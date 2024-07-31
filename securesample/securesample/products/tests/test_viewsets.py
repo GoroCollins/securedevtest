673d2ffc37735b460d6f7c41748bb9ba980103bb
Goro Collins <gorocollins@gmail.com>
2024-07-31 22:04:19 +0300

    Added more tests for the viewsets

--- securesample/securesample/products/tests/test_viewsets.py
+++ securesample/securesample/products/tests/test_viewsets.py
@@ -77,6 +77,21 @@ def test_delete_category(api_client, user, category):
     assert response.status_code == status.HTTP_204_NO_CONTENT
     assert Category.objects.count() == 0
 
+@pytest.mark.django_db
+def test_list_categories(api_client, category):
+    url = reverse('api:category-list')
+    response = api_client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.data) == 1
+
+@pytest.mark.django_db
+def test_create_category_with_missing_fields(api_client, user):
+    api_client.force_authenticate(user=user)
+    url = reverse('api:category-list')
+    data = {'code': 'men'}
+    response = api_client.post(url, data, format='json')
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
 # Tests for ShoeViewSet
 
 @pytest.mark.django_db
@@ -124,6 +139,27 @@ def test_delete_shoe(api_client, user, shoe):
     assert response.status_code == status.HTTP_204_NO_CONTENT
     assert Shoe.objects.count() == 0
 
+@pytest.mark.django_db
+def test_list_shoes(api_client, shoe):
+    url = reverse('api:shoe-list')
+    response = api_client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.data) == 1
+
+@pytest.mark.django_db
+def test_create_shoe_with_invalid_price(api_client, user, category):
+    api_client.force_authenticate(user=user)
+    url = reverse('api:shoe-list')
+    data = {
+        'category': category.code,
+        'name': 'Invalid Shoe',
+        'description': 'Invalid price',
+        'price': 'invalid',
+        'quantity': 5
+    }
+    response = api_client.post(url, data, format='json')
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    
 # Tests for ShoeImageViewSet
 
 def create_image_file():
@@ -182,3 +218,21 @@ def test_delete_shoe_image(api_client, user, shoe_image):
     response = api_client.delete(url)
     assert response.status_code == status.HTTP_204_NO_CONTENT
     assert ShoeImage.objects.count() == 0
+    
+@pytest.mark.django_db
+def test_list_shoe_images(api_client, shoe_image):
+    url = reverse('api:shoeimage-list')
+    response = api_client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.data) == 1
+
+@pytest.mark.django_db
+def test_create_shoe_image_with_invalid_file(api_client, user, shoe):
+    api_client.force_authenticate(user=user)
+    url = reverse('api:shoeimage-list')
+    data = {
+        'shoe': shoe.id,
+        'image': 'invalid_image_file'
+    }
+    response = api_client.post(url, data, format='multipart')
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
