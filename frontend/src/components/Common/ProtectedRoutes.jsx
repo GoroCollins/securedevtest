8f82c14d6d4d6783f00ff8694a4358a85bf9fd34
Goro Collins <gorocollins@gmail.com>
2024-08-04 17:43:44 +0300

    Added import for React and removed commented code

--- frontend/src/components/Common/ProtectedRoutes.jsx
+++ frontend/src/components/Common/ProtectedRoutes.jsx
@@ -1,15 +1,4 @@
-// import { Navigate, useLocation } from "react-router-dom";
-// import { authService } from "./Auth.Service"
-
-// export const ProtectedRoute = ({ children }) => {
-//   const location = useLocation();
-//   // if (!token) {
-//   //   // user is not authenticated
-//   //   // return <Navigate to="/signin" />;
-//   //   return <Navigate to="/" replace state={{ from: location }} />;
-//   // }
-//   return children;
-// };
+import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { useAuthService } from "./Auth.Service";
 
