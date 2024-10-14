0017e65a0f10d1e1429f51e246203b6863484ab9
Goro Collins <gorocollins@gmail.com>
2024-10-14 19:13:21 +0300

    Increased the initial count to 4 from 0

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import HomePage from './components/HomePage';
 import { AuthProvider } from '../src/components/Common/Auth.Service'; // Make sure to import AuthProvider
 
 function App() {
-  const [count, setCount] = useState<number>(0);
+  const [count, setCount] = useState<number>(4);
 
   return (
     <AuthProvider>  {/* Wrap your entire app inside AuthProvider */}
