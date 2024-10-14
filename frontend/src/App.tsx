717b2eb9a35b4f771c2065e5ddc9a9baf634ce55
Goro Collins <gorocollins@gmail.com>
2024-10-14 19:18:08 +0300

    Wrapped the App using BrowserRouter from react-router-dom package to provide routing context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,18 @@ import { useState } from 'react';
 import './App.css';
 import HomePage from './components/HomePage';
 import { AuthProvider } from '../src/components/Common/Auth.Service'; // Make sure to import AuthProvider
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState<number>(4);
 
   return (
-    <AuthProvider>  {/* Wrap your entire app inside AuthProvider */}
+    <Router>
+      <AuthProvider>  {/* Wrap your entire app inside AuthProvider */}
       <HomePage count={count} setCount={setCount} />
     </AuthProvider>
+    </Router>
+    
   );
 }
 
