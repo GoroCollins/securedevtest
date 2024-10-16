81cade94441ea8ed291b6832c386691a090f3599
Goro Collins <gorocollins@gmail.com>
2024-10-16 20:53:00 +0300

    Added NavBar component to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { AuthProvider } from '../src/components/Common/Auth.Service'; // Make sure to import AuthProvider
 import { BrowserRouter as Router } from 'react-router-dom';
 import RoutesConfig from './components/Common/RoutesConfig';
+import NavBar from './components/Common/NavBar';
 
 function App() {
   const [count, setCount] = useState<number>(4);
@@ -10,6 +11,7 @@ function App() {
   return (
     <Router>
       <AuthProvider>  {/* Wrap your entire app inside AuthProvider */}
+        <NavBar />
         <RoutesConfig count={count} setCount={setCount} />
       </AuthProvider>
     </Router>
